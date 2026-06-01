474109fe1be4d0948a572eb76c4e053a5ed03d35
ravenbix <hankswart@gmail.com>
2026-06-01 17:08:11 +0200

    test: strengthen Get-SsisOperationObject tests

--- tests/Unit/Private/Get-SsisOperationObject.tests.ps1
+++ tests/Unit/Private/Get-SsisOperationObject.tests.ps1
@@ -11,8 +11,11 @@ Describe 'Get-SsisOperationObject' {
     It 'Returns the whole Operations collection when no id is given' {
         InModuleScope $script:moduleName {
             $catalog = [PSCustomObject]@{ Operations = @('op1', 'op2') }
+
             $result = Get-SsisOperationObject -Catalog $catalog
-            $result | Should -Be @('op1', 'op2')
+            $result | Should -HaveCount 2
+            $result[0] | Should -Be 'op1'
+            $result[1] | Should -Be 'op2'
         }
     }
 
